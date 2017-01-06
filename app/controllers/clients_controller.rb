2cc38b345baff8ed7a15b10bfe43605c24ca5ed2
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2017-01-07 00:34:29 +0100

    implement initial client show

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,16 @@
 class ClientsController < ApplicationController
+  before_action :set_client, only: [:show]
+
   def index
     @clients = current_user.clients
   end
+
+  def show
+  end
+
+  private
+
+    def set_client
+      @client = Client.find(params[:id])
+    end
 end

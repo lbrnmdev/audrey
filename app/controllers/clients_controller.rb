b771d26f5bdcd60b447154ed642c40af865b445f
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2017-01-08 04:33:32 +0100

    implement adding clients

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -8,9 +8,27 @@ class ClientsController < ApplicationController
   def show
   end
 
+  def new
+    @client = current_user.clients.new
+  end
+
+  def create
+    @client = current_user.clients.new(client_params)
+    if @client.save
+      flash[:success] = "New client, #{@client.lastname} added!"
+      redirect_to @client
+    else
+      render 'new'
+    end
+  end
+
   private
 
     def set_client
       @client = Client.find(params[:id])
+    end
+
+    def client_params
+      params.require(:client).permit(:lastname, :other_names, :telephone, :email, :address)
     end
 end

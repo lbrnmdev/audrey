647c2001c0e5988170dc90cd84518729ccdbbaf4
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2017-02-27 13:02:37 +0000

    vehicles index only shows vehicles belonging to current user's clients

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -2,6 +2,6 @@ class VehiclesController < ApplicationController
   def index
     # should this be vehicles specific to current user or the whole system?
     # something like current_user.vehicles or current_user.clients.vehicles
-    @vehicles = Vehicle.all
+    @vehicles = current_user.vehicles
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :clients
+  has_many :vehicles, through: :clients
 end

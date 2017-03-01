434249fca6a9555978e6fe7142a98f328027ea56
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2017-03-01 15:46:08 +0000

    implement preliminary vehicle show card

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,7 +1,19 @@
 class VehiclesController < ApplicationController
+  before_action :set_vehicle, only: [:show]
+
   def index
     # should this be vehicles specific to current user or the whole system?
     # something like current_user.vehicles or current_user.clients.vehicles
     @vehicles = current_user.vehicles
   end
+
+  def show
+  end
+
+  private
+
+    def set_vehicle
+      @vehicle = Vehicle.find(params[:id])
+      check_user_authorization_for @vehicle
+    end
 end

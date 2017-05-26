43d93ddde99aa50eb37dae10b3587e2053a64860
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2017-05-26 12:14:54 +0200

    implement preliminary insurer create

--- app/controllers/insurers_controller.rb
+++ app/controllers/insurers_controller.rb
@@ -12,10 +12,24 @@ class InsurersController < ApplicationController
     @insurer = current_user.insurers.new
   end
 
+  def create
+    @insurer = current_user.insurers.new(insurer_params)
+    if @insurer.save
+      flash[:success] = "#{@insurer.full_legal_name} has been added as an insurance company"
+      redirect_to @insurer
+    else
+      render 'new'
+    end
+  end
+
   private
 
     def set_insurer
       @insurer = Insurer.find(params[:id])
       check_user_authorization_for @insurer
+    end
+
+    def insurer_params
+      params.require(:insurer).permit(:name, :full_legal_name, :address, :email_address, :phone_number)
     end
 end

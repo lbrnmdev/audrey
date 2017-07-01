a38817475f7d052f113c0ac105fb16b86c146fdb
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2017-07-01 13:09:05 +0200

    improve policy status rendering in tviews

--- app/helpers/policies_helper.rb
+++ app/helpers/policies_helper.rb
@@ -1,2 +1,25 @@
 module PoliciesHelper
+  # return cell class depending on policy status
+  def policy_table_cell_class policy
+    if policy.expired?
+      'negative'
+    elsif (policy.not_issued? || policy.cancelled?)
+      'warning'
+    else
+      'positive'
+    end
+  end
+
+  # return cell popup content depending on policy status
+  def policy_cell_pop_up policy
+    if policy.expired?
+      "Expired: " + policy.end_date.to_s(:rfc822)
+    elsif policy.not_issued?
+      "Not issued"
+    elsif policy.cancelled?
+      "Cancelled"
+    else
+      "Valid from " + policy.start_date.to_s(:rfc822) + " to " + policy.end_date.to_s(:rfc822)
+    end
+  end
 end

c540ed5d7af58e88927efefe67ad1bd517fe1579
lbrnm dev macbook-pro <lbrnm.dev@gmail.com>
2017-08-07 00:57:42 +0200

    add vehicles to overview

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -2,5 +2,6 @@ class OverviewController < ApplicationController
   def home
     @clients = current_user.clients
     @policies = current_user.policies
+    @vehicles = current_user.vehicles
   end
 end
